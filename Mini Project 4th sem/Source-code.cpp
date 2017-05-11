1133f928cc5346f58ceabd77c0a7797a837dd467
rohitthapliyal2000 <rohitthapliyal2000@gmail.com>
2017-05-11 23:22:42 +0530

    Add files via upload

--- Mini Project 4th sem/Source-code.cpp
+++ Mini Project 4th sem/Source-code.cpp
@@ -1,9 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr[100][100];
+int arr[100][100], range;
 
-
+//Function to check if the input is valid
 bool isNumber(string s)
 {
     bool result = true;
@@ -15,44 +15,168 @@ bool isNumber(string s)
     return result;
 }
 
-void input()
+//Function to input the range and matrix
+void Input()
 {
     string arrayElements;
     string n;
-    int range;
     try
     {
         cin >> n;
+
+        //If input is not numeric
         if (!isNumber(n))
             throw n;
+
+        //If input is numeric but out of bounds
         if (atoi(n.c_str()) < 0 || atoi(n.c_str()) > 100)
             throw atoi(n.c_str());
+
+        //Input is correct, assigning value to range
         range = atoi(n.c_str());
         for (int i = 0; i < range; i++)
         {
             for(int j = 0; j < range; j++)
             {
                 cin >> arrayElements;
+
+                //If input is not numeric
                 if (!isNumber(arrayElements))
                     throw arrayElements;
+
+                //If input is numeric but out of bounds
                 if (atoi(arrayElements.c_str()) < 0 || atoi(arrayElements.c_str()) > 2)
                     throw atoi(arrayElements.c_str());
+
+                //Input is correct, assigning value to array
                 arr[i][j] = atoi(arrayElements.c_str());
             }
         }
     }
+
+    //Input is not numeric
     catch(string str)
     {
         cout << str << " is an Invalid Entry" << endl;
     }
+
+    //Input is out of bounds
     catch(int num)
     {
         cout << num << " is out of bounds" << endl;
     }
 }
 
+//Function to check if there is a path of 1s from first row to last.
+bool onePath()
+{
+    int i = 0, j = 0, oneFinder = 0, elementIncrement = 2;
+    for (i = 1; i < range; i++)
+    {
+        for (j = 0; j < range; j++)
+        {
+            if (arr[i][j] == 1)
+            {
+                if (j == 0)
+                {
+                    if (arr[i-1][j] == (i+oneFinder) || arr[i-1][j+1] == (i+oneFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+                else if (j == range-1)
+                {
+                    if (arr[i-1][j] == (i+oneFinder) || arr[i-1][j-1] == (i+oneFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+                else
+                {
+                    if (arr[i-1][j] == (i+oneFinder) || arr[i-1][j-1] == (i+oneFinder) || arr[i-1][j+1] == (i+oneFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+            }
+        }
+        oneFinder++;
+        elementIncrement += 2;
+    }
+    for (i = range-1, j = 0; j < range; j++)
+    {
+        if (arr[i][j] == (2*(range-1)+1))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+//Function to check if there is a path of 2s from first column to last.
+bool twoPath()
+{
+    int i = 0, j = 0, twoFinder = 1, elementIncrement = 2;
+    for (j = 1; j < range; j++)
+    {
+        for (i = 0; i < range; i++)
+        {
+            if (arr[i][j] == 2)
+            {
+                if (i == 0)
+                {
+                    if (arr[i+1][j-1] == (j+twoFinder) || arr[i][j-1] == (j+twoFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+                else if (i == range-1)
+                {
+                    if (arr[i][j-1] == (j+twoFinder) || arr[i-1][j-1] == (j+twoFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+                else
+                {
+                    if (arr[i][j-1] == (j+twoFinder) || arr[i-1][j-1] == (j+twoFinder) || arr[i+1][j-1] == (j+twoFinder))
+                    {
+                        arr[i][j] = arr[i][j] + elementIncrement;
+                    }
+                }
+            }
+        }
+        twoFinder++;
+        elementIncrement += 2;
+    }
+    for( j = range-1, i = 0; i < range; i++)
+    {
+        if (arr[i][j] == (2*range))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 //Driver Code
 int main()
 {
-    input();
+    Input();
+    if (onePath() && twoPath())
+    {
+        cout << "Ambiguous" << endl;
+    }
+    else if (twoPath())
+    {
+        cout <<"2" << endl;
+    }
+    else if (onePath())
+    {
+        cout << "1" << endl;
+    }
+    else
+    {
+        cout << "0" << endl;
+    }
 }
